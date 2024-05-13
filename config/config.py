1e3d44b7243eaf42de8481dbf549946bfebb9f68
dungpham <edward1242002@gmail.com>
2024-05-13 22:16:21 +0700

    first commit

---
+++ config/config.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @File : config.py
+# @Author: Runist
+# @Time : 2020-09-11 13:54
+# @Software: PyCharm
+# @Brief: 配置文件
+
+rpn_lr = 1e-4
+cls_lr = 1e-5
+
+epoch = 50
+
+anchor_box_scales = [128, 256, 512]
+anchor_box_ratios = [[1, 1], [1, 2], [2, 1]]
+
+batch_size = 16
+rpn_stride = 16
+input_shape = (378, 378)
+share_layer_shape = (round(input_shape[0] / rpn_stride), round(input_shape[1] / rpn_stride))
+
+
+num_rois = 128
+num_regions = 256
+
+rpn_min_overlap = 0.3
+rpn_max_overlap = 0.7
+classifier_min_overlap = 0.1
+classifier_max_overlap = 0.5
+classifier_regr_std = [8.0, 8.0, 4.0, 4.0]
+
+data_pretreatment = 'random'
+
+#annotation_path = "./config/train.txt"
+label = ['nlb', 'nls', 'gls']
+
+num_classes = len(label) + 1
