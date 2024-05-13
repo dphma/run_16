6c53e65652f88c6fa32a3d063f710e624274729f
dphma <anhdung1242002@gmail.com>
2024-05-14 01:36:29 +0700

    Update get_dr_txt.py

--- mAP/get_dr_txt.py
+++ mAP/get_dr_txt.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# @File : get_dr_txt.py
-# @Author: Runist
-# @Time : 2020/5/8 14:27
-# @Software: PyCharm
-# @Brief: 获取预测框的数据，并转mAP的txt检测格式
 from predict import FasterRCNN
 from PIL import Image
 import config.config as cfg
@@ -26,9 +20,9 @@ class Frcnn(FasterRCNN):
 
         resize_image = self.process_image(image)
         old_w, old_h = image.size
-        # 计算特征层下的宽度、高度
+        
         new_h, new_w = np.shape(resize_image)[1:-1]
-        # 计算图片输入到rpn的输出shape，[-1]是因为rpn的输出list，最后一个是共享特征层
+        
         rpn_height, rpn_width = self.model_rpn.compute_output_shape((1, new_h, new_w, 3))[-1][1:-1]
 
         predict_rpn = self.model_rpn(resize_image, training=False)
@@ -60,7 +54,7 @@ class Frcnn(FasterRCNN):
         labels = []
 
         for i in range(predict_boxes.shape[0] // cfg.num_rois + 1):
-            # 把predict_boxes分成多个(128，4）
+            
             rois = predict_boxes[cfg.num_rois * i: cfg.num_rois * (i+1), :]
             rois = np.expand_dims(rois, axis=0)
 
@@ -68,7 +62,6 @@ class Frcnn(FasterRCNN):
                 print("Your model can't detect any bounding boxes.")
                 exit(0)
 
-            # 把不够128框的部分，补足128个
             if i == predict_boxes.shape[0] // cfg.num_rois:
 
                 curr_shape = rois.shape
@@ -76,9 +69,9 @@ class Frcnn(FasterRCNN):
                 target_shape = (curr_shape[0], cfg.num_rois, curr_shape[2])
 
                 rois_padded = np.zeros(target_shape).astype(rois.dtype)
-                # 前面框足够的部分不变
+                
                 rois_padded[:, :curr_shape[1], :] = rois
-                # 后面不够的地方用同一个数据补足
+                
                 rois_padded[0, curr_shape[1]:, :] = rois[0, 0, :]
 
                 rois = rois_padded
@@ -86,34 +79,30 @@ class Frcnn(FasterRCNN):
             p_cls, p_regr = self.model_classifier([share_layer, rois], training=False)
 
             for j in range(p_cls.shape[1]):
-                # 如果这个框置信度都小于阈值，那就直接跳过了
+                
                 conf = np.max(p_cls[0, j, :-1])
                 if conf < self.confidence:
                     continue
 
                 x, y, w, h = rois[0, j, :]
 
-                # 获取最大分类值的索引
                 cls_num = np.argmax(p_cls[0, j, :-1])
 
                 tx, ty, tw, th = p_regr[0, j, 4 * cls_num: 4 * (cls_num + 1)]
 
-                # 在当时有乘上一个系数，现在就要除回去
                 tx /= cfg.classifier_regr_std[0]
                 ty /= cfg.classifier_regr_std[1]
                 tw /= cfg.classifier_regr_std[2]
                 th /= cfg.classifier_regr_std[3]
 
-                # 左上角坐标变换为中心坐标
                 cx = x + w / 2.0
                 cy = y + h / 2.0
-                # 论文公式的逆变换
+                
                 cx1 = tx * w + cx
                 cy1 = ty * h + cy
                 w1 = np.exp(tw) * w
                 h1 = np.exp(th) * h
 
-                # 再转换回左上角和右下角的坐标
                 x1 = cx1 - w1 / 2.0
                 y1 = cy1 - h1 / 2.0
                 x2 = cx1 + w1 / 2.0
@@ -131,12 +120,10 @@ class Frcnn(FasterRCNN):
         if len(boxes) == 0:
             return image
 
-        # 筛选出其中得分高于confidence的框
         labels = np.array(labels)
         probs = np.array(probs)
         boxes = np.array(boxes, dtype=np.float32)
 
-        # 变换为标准尺度的框
         boxes[:, 0] = boxes[:, 0] * cfg.rpn_stride / new_w
         boxes[:, 1] = boxes[:, 1] * cfg.rpn_stride / new_h
         boxes[:, 2] = boxes[:, 2] * cfg.rpn_stride / new_w
@@ -148,7 +135,7 @@ class Frcnn(FasterRCNN):
             mask = labels == c
 
             if len(probs[mask]) > 0:
-                # 取出得分高于confidence_threshold的框
+    
                 boxes_to_process = boxes[mask]
                 score_to_process = probs[mask]
 
@@ -211,9 +198,7 @@ if __name__ == '__main__':
         image_id = os.path.basename(image_path)[:-4]
 
         image = Image.open(image_path)
-        # image = Image.open(image_path)
-        # 开启后在之后计算mAP可以可视化
-        # image.save("./input/images-optional/"+image_id+".jpg")
+       
         frcnn.detect_single_image(image, image_id)
 
     print("Conversion completed!")
