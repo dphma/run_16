42a7130e4de1cf77baf47fcdf7248d8a7e74096a
dphma <anhdung1242002@gmail.com>
2024-05-23 00:12:16 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ cls_lr = 1e-5
 
 epoch = 50
 
-anchor_box_scales = [64, 128, 256]
+anchor_box_scales = [128, 256, 512]
 anchor_box_ratios = [[1, 1], [1, 2], [2, 1]]
 
 batch_size = 4
@@ -16,7 +16,7 @@ num_rois = 128
 num_regions = 256
 
 rpn_min_overlap = 0.3
-rpn_max_overlap = 0.7
+rpn_max_overlap = 0.8
 classifier_min_overlap = 0.1
 classifier_max_overlap = 0.5
 classifier_regr_std = [8.0, 8.0, 4.0, 4.0]
